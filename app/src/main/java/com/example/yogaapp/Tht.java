e2f753281ae41a13a30f0e9ab9ec6a1000d758b4
Lylla <thanhthuylss99@gmail.com>
2021-11-16 16:00:36 +0700

    first commit

---
+++ app/src/main/java/com/example/yogaapp/Tht.java
@@ -0,0 +1,27 @@
+package com.example.yogaapp;
+
+public class Tht {
+    public String idTht;
+    public String nameTht;
+
+    public Tht(String idTht, String nameTht) {
+        this.idTht = idTht;
+        this.nameTht = nameTht;
+    }
+
+    public String getIdTht() {
+        return idTht;
+    }
+
+    public void setIdTht(String idTht) {
+        this.idTht = idTht;
+    }
+
+    public String getNameTht() {
+        return nameTht;
+    }
+
+    public void setNameTht(String nameTht) {
+        this.nameTht = nameTht;
+    }
+}
